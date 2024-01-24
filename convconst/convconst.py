c320d33c55e0260116267acd7146fc91475ccb2c
Xiaoyue Chen <xchen@vvvu.org>
2024-01-24 03:12:18 +0100

    Remove mantissa option

--- convconst/convconst.py
+++ convconst/convconst.py
@@ -9,12 +9,8 @@ def parse():
     parser = argparse.ArgumentParser(
         prog="convconst",
         description="Extract constants from convolutional neural networks.")
-
     parser.add_argument("model", help="the onnx model file")
     parser.add_argument("dest", help="destination directory")
-    parser.add_argument("-m", "--mantissa", action="store_true",
-                        help="only extract the mantissa")
-
     return parser.parse_args()
 
 
@@ -30,24 +26,13 @@ def extract_conv_weights(model):
 
     weight = [onnx.numpy_helper.to_array(init) for init in conv_initializer]
 
-    for w in weight:
-        assert w.dtype == np.float32
-
     return weight
-
-
-def mantissa(x):
-    assert x.dtype == np.float32
-    mantissa, _ = np.frexp(x)
-    return np.absolute(mantissa * 2 ** 24).astype(int)
 
 
 def main():
     args = parse()
     model = onnx.load(args.model)
     weight = extract_conv_weights(model)
-    if args.mantissa:
-        weight = [mantissa(w) for w in weight]
     dest = Path(args.dest)
     os.makedirs(dest, exist_ok=True)
     for w in weight:
